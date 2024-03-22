2d42f8be4102bd9a7b6d4186dffcd7af7c2f1b9c
ScriptJediMaster <164449858+ScriptJediMaster@users.noreply.github.com>
2024-03-22 11:38:38 +0700

    Update profile mahasiswa.py

--- profile mahasiswa.py
+++ profile mahasiswa.py
@@ -1,17 +1,16 @@
 def tampilkan_profil_mahasiswa(Nama, NIM, Prodi, Hobi):
-    print("[91mProfile Mahasiswa UNJAYA YOGYAKARTA")
-    print("Mahasiswa Prodi {0}  UNJAYA YOGYAKARTA".format(Prodi))
+    print("[91mProfile Mahasiswa UNIV UNJAYA YOGYAKARTA")
+    print("Mahasiswa Prodi {0}  UNIV UNJAYA YOGYAKARTA".format(Prodi))
     print("Dengan nama {0}".format(Nama))
     print("Mempunyai NIM {0}".format(NIM))
     print("Memiliki Hobi {0}".format(Hobi))
 
-# Data Mahasiswa
+
 data_mahasiswa = {
-    'Nama': 'Restu Anggit Gumilar',
+    'Nama': 'RESTU ANGGIT GUMILAR',
     'NIM': '232102009',
-    'Prodi': 'Teknik Informatika',
-    'Hobi': 'Memancing'
+    'Prodi': 'S1 INFORMATIKA',
+    'Hobi': 'REBAHAN SANTUY'
 }
 
-# Menampilkan profil mahasiswa
 tampilkan_profil_mahasiswa(**data_mahasiswa)
